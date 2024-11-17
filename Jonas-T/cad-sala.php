e71272db6cf60b563ced60fcb450082bde7ce046
Elen Freire <elenfreire63@gmail.com>
2024-11-17 19:05:45 -0300

    grrrrrrr css concertado/quebrado cad-user e cad-sala add

---
+++ Jonas-T/cad-sala.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Definindo o conjunto de caracteres para UTF-8, adequado para português -->
+    <meta charset="UTF-8">
+
+    <!-- Tornando o site responsivo em dispositivos móveis -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link para o arquivo de estilos (CSS) local -->
+    <link rel="stylesheet" href="cad-sala.css">
+    <!-- Link para o Bootstrap 5 (framework CSS) que oferece uma estrutura de layout responsiva e estilizada -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- Link para os ícones do Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- Título da página exibido na aba do navegador -->
+    <title>Cadastro de Salas</title>
+</head>
--- Jonas-T/cad-user.php
+++ Jonas-T/cad-user.php
@@ -8,13 +8,13 @@
     <!-- Tornando o site responsivo em dispositivos móveis -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Link para o arquivo de estilos (CSS) local -->
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="cad-user.css">
     <!-- Link para o Bootstrap 5 (framework CSS) que oferece uma estrutura de layout responsiva e estilizada -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <!-- Link para os ícones do Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!-- Título da página exibido na aba do navegador -->
-    <title>Reúne Aqui</title>
+    <title>Cadastre-se</title>
 </head>
 
 <body>
