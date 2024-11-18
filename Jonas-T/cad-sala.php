2ed7884bee565efddfd9130e2222f08110f25ab8
Elen Freire <elenfreire63@gmail.com>
2024-11-18 19:31:55 -0300

    upd

--- Jonas-T/cad-sala.php
+++ Jonas-T/cad-sala.php
@@ -8,7 +8,7 @@
     <!-- Tornando o site responsivo em dispositivos móveis -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Link para o arquivo de estilos (CSS) local -->
-    <link rel="stylesheet" href="cad-sala.css">
+    <link rel="stylesheet" href="home.css">
     <!-- Link para o Bootstrap 5 (framework CSS) que oferece uma estrutura de layout responsiva e estilizada -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <!-- Link para os ícones do Bootstrap -->
